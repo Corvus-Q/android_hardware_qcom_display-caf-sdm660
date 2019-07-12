0fe56d8f876afad61963769ec2c7307824e034cd
Rizky Benggolo <travarilo@gmail.com>
2019-07-12 04:00:30 +0000

    display: use omnirom's pathmap

--- Android.mk
+++ Android.mk
@@ -1,5 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-display))
-
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-sdm660)
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
@@ -21,5 +20,4 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
-
 endif
